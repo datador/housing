ac5113f05b0e47bfc862facbc70d780c06752414
datador <halldor95k@gmail.com>
2025-03-23 20:06:00 +0000

    fix linting

--- app/imports/import_hms.py
+++ app/imports/import_hms.py
@@ -1,7 +1,8 @@
 import csv
-from sqlalchemy import select, delete
-from app.db import Session, Model, engine
+from sqlalchemy import delete
+from app.db import Session
 from app.models.hms import HMS
+
 
 def main():
     with Session() as session:
@@ -11,12 +12,13 @@ def main():
     with Session() as session:
         with session.begin():
             # https://frs3o1zldvgn.objectstorage.eu-frankfurt-1.oci.customer-oci.com/n/frs3o1zldvgn/b/public_data_for_download/o/kaupskra.csv
-            with open('app/data/kaupskra.csv', encoding='utf-8') as f:
+            with open("app/data/kaupskra.csv", encoding="utf-8") as f:
                 reader = csv.DictReader(f)
 
                 for row in reader:
                     house = HMS(**row)
                     session.add(house)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
