a1316574f4ba0324cffe1df1cbbd55c6949d8533
datador <halldor95k@gmail.com>
2025-03-25 18:09:27 +0000

    add raw table

--- app/models/hms.py
+++ app/models/hms.py
@@ -14,9 +14,50 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.db import Model
 
 
+class HMSRaw(Model):
+    __tablename__ = "hms_raw"
+    __table_args__ = (
+        PrimaryKeyConstraint("faerslunumer", "fastnum"),
+        UniqueConstraint("faerslunumer", "fastnum", name="uq_raw_faerslunumer_fastnum"),
+        {"schema": "raw"},
+    )
+
+    faerslunumer: Mapped[str] = mapped_column(String, primary_key=True, index=True)
+    emnr: Mapped[str] = mapped_column(String)
+    skjalanumer: Mapped[str] = mapped_column(String)
+    fastnum: Mapped[str] = mapped_column(String, primary_key=True)
+    heimilisfang: Mapped[Optional[str]] = mapped_column(String)
+    postnr: Mapped[Optional[str]] = mapped_column(String)
+    heinum: Mapped[Optional[str]] = mapped_column(String)
+    svfn: Mapped[str] = mapped_column(String)
+    sveitarfelag: Mapped[str] = mapped_column(String)
+    utgdag: Mapped[str] = mapped_column(String)
+    thinglystdags: Mapped[str] = mapped_column(String)
+    kaupverd: Mapped[str] = mapped_column(String)
+    fasteignamat: Mapped[str] = mapped_column(String)
+    fasteignamat_gildandi: Mapped[Optional[str]] = mapped_column(String)
+    fyrirhugad_fasteignamat: Mapped[Optional[str]] = mapped_column(String)
+    brunabotamat_gildandi: Mapped[Optional[str]] = mapped_column(String)
+    byggar: Mapped[str] = mapped_column(String)
+    fepilog: Mapped[str] = mapped_column(String)
+    einflm: Mapped[str] = mapped_column(String)
+    lod_flm: Mapped[Optional[str]] = mapped_column(String)
+    lod_flmein: Mapped[Optional[str]] = mapped_column(String)
+    fjherb: Mapped[Optional[str]] = mapped_column(String)
+    tegund: Mapped[str] = mapped_column(String)
+    fullbuid: Mapped[str] = mapped_column(String)
+    onothaefur_samningur: Mapped[str] = mapped_column(String)
+
+    def __repr__(self):
+        return f'HMSRaw("{self.faerslunumer}", "{self.fastnum}")'
+
+
 class HMS(Model):
     __tablename__ = "hms"
-
+    __table_args__ = (
+        PrimaryKeyConstraint("faerslunumer", "fastnum"),
+        UniqueConstraint("faerslunumer", "fastnum", name="uq_faerslunumer_fastnum"),
+    )
     faerslunumer: Mapped[str] = mapped_column(
         String(6), index=True, nullable=False, primary_key=True
     )
@@ -45,11 +86,6 @@ class HMS(Model):
     fullbuid: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     onothaefur_samningur: Mapped[int] = mapped_column(
         Integer, default=0, nullable=False
-    )
-
-    __table_args__ = (
-        PrimaryKeyConstraint("faerslunumer", "fastnum"),
-        UniqueConstraint("faerslunumer", "fastnum", name="uq_faerslunumer_fastnum"),
     )
 
     def __repr__(self):
